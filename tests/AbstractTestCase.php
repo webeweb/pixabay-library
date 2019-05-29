cac3bc257d0170e41748975fdc4e4a9b81a8dff8
webeweb <support@webeweb.fr>
2019-05-29 18:33:20 +0200

    Add PHPUnit

---
+++ tests/AbstractTestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the pixabay-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pixabay\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Abstract test case.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pixabay\Tests
+ * @abstract
+ */
+abstract class AbstractTestCase extends TestCase {
+
+}
