e98c35ccf340b7143ee9140a626dc1591a3357be
webeweb <support@webeweb.fr>
2019-05-29 18:34:25 +0200

    Add Rate limit trait

---
+++ tests/Fixtures/Traits/TestRateLimitTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the pixabay-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pixabay\Tests\Fixtures\Traits;
+
+use WBW\Library\Pixabay\Traits\RateLimitTrait;
+
+/**
+ * Test rate limit trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pixabay\Tests\Fixtures\Traits
+ */
+class TestRateLimitTrait {
+
+    use RateLimitTrait;
+}
