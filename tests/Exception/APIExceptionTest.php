b3c43537222f8720f96daad6c80b3e9315f789ce
webeweb <support@webeweb.fr>
2019-05-29 18:34:01 +0200

    Add API exception

---
+++ tests/Exception/APIExceptionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the pixabay-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pixabay\Tests\Exception;
+
+use Exception;
+use WBW\Library\Pixabay\Exception\APIException;
+use WBW\Library\Pixabay\Tests\AbstractTestCase;
+
+/**
+ * API exception test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pixabay\Tests\Exception
+ */
+class APIExceptionTest extends AbstractTestCase {
+
+    /**
+     * Tests the construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        // Set an Exception mock.
+        $arg = new Exception;
+
+        $obj = new APIException("message", $arg);
+
+        $this->assertEquals("message", $obj->getMessage());
+        $this->assertSame($arg, $obj->getPrevious());
+    }
+}
