70216e721535dd5b340815fa0d8d4eb92eda1039
webeweb <support@webeweb.fr>
2019-06-01 10:41:52 +0200

    Add Search videos response

---
+++ src/Model/Response/SearchVideosResponse.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the pixabay-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pixabay\Model\Response;
+
+use WBW\Library\Pixabay\Model\AbstractResponse;
+use WBW\Library\Pixabay\Model\VideoHit;
+
+/**
+ * Search videos response.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pixabay\Model\Response
+ */
+class SearchVideosResponse extends AbstractResponse {
+
+    /**
+     * Add an video hit.
+     *
+     * @param VideoHit $videoHit The video hit.
+     * @return SearchVideosResponse Returns this search video response.
+     */
+    public function addVideoHit(VideoHit $videoHit) {
+        return $this->addHit($videoHit);
+    }
+
+    /**
+     * Get the videos hits.
+     *
+     * @return VideoHit[] Returns the video hits.
+     */
+    public function getVideoHits() {
+        return $this->getHits();
+    }
+}
---
+++ tests/Model/Response/SearchVideosResponseTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the pixabay-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pixabay\Tests\Model\Response;
+
+use WBW\Library\Pixabay\Model\Response\SearchVideosResponse;
+use WBW\Library\Pixabay\Model\VideoHit;
+use WBW\Library\Pixabay\Tests\AbstractTestCase;
+
+/**
+ * Search videos response test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pixabay\Tests\Model\Response
+ */
+class SearchVideosResponseTest extends AbstractTestCase {
+
+    /**
+     * Tests the addVideoHit() method.
+     *
+     * @return void
+     */
+    public function testAddVideoHit() {
+
+        // Set an Video hit mock.
+        $videoHit = new VideoHit();
+
+        $obj = new SearchVideosResponse();
+
+        $obj->addVideoHit($videoHit);
+        $this->assertCount(1, $obj->getVideoHits());
+        $this->assertSame($videoHit, $obj->getVideoHits()[0]);
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new SearchVideosResponse();
+
+        $this->assertCount(0, $obj->getVideoHits());
+    }
+}
