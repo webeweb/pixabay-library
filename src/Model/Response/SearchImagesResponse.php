12b831c6f950920651190b1d8fdf33a32189d22c
webeweb <support@webeweb.fr>
2019-06-01 10:41:39 +0200

    Add Search images response

---
+++ src/Model/Response/SearchImagesResponse.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the pixabay-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pixabay\Model\Response;
+
+use WBW\Library\Pixabay\Model\AbstractResponse;
+use WBW\Library\Pixabay\Model\ImageHit;
+
+/**
+ * Search images response.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pixabay\Model\Response
+ */
+class SearchImagesResponse extends AbstractResponse {
+
+    /**
+     * Add an image hit.
+     *
+     * @param ImageHit $imageHit The image hit.
+     * @return SearchImagesResponse Returns this search image response.
+     */
+    public function addImageHit(ImageHit $imageHit) {
+        return $this->addHit($imageHit);
+    }
+
+    /**
+     * Get the images hits.
+     *
+     * @return ImageHit[] Returns the image hits.
+     */
+    public function getImageHits() {
+        return $this->getHits();
+    }
+}
---
+++ tests/Model/Response/SearchImagesResponseTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the pixabay-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pixabay\Tests\Model\Response;
+
+use WBW\Library\Pixabay\Model\ImageHit;
+use WBW\Library\Pixabay\Model\Response\SearchImagesResponse;
+use WBW\Library\Pixabay\Tests\AbstractTestCase;
+
+/**
+ * Search images response test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pixabay\Tests\Model\Response
+ */
+class SearchImagesResponseTest extends AbstractTestCase {
+
+    /**
+     * Tests the addImageHit() method.
+     *
+     * @return void
+     */
+    public function testAddImageHit() {
+
+        // Set an Image hit mock.
+        $imageHit = new ImageHit();
+
+        $obj = new SearchImagesResponse();
+
+        $obj->addImageHit($imageHit);
+        $this->assertCount(1, $obj->getImageHits());
+        $this->assertSame($imageHit, $obj->getImageHits()[0]);
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new SearchImagesResponse();
+
+        $this->assertCount(0, $obj->getImageHits());
+    }
+}
