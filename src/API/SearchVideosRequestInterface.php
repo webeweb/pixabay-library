72947860fcee794cbae207b2e9d30fb351119a44
webeweb <support@webeweb.fr>
2019-06-03 19:53:31 +0200

    Add Search videos request interface

---
+++ src/API/SearchVideosRequestInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the pixabay-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pixabay\API;
+
+/**
+ * Search videos request interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pixabay\API
+ */
+interface SearchVideosRequestInterface extends RequestInterface {
+
+    /**
+     * Video type "all".
+     *
+     * @var string
+     */
+    const VIDEO_TYPE_ALL = "all";
+    /**
+     * Video type "animation".
+     *
+     * @var string
+     */
+    const VIDEO_TYPE_ANIMATION = "animation";
+    /**
+     * Video type "film".
+     *
+     * @var string
+     */
+    const VIDEO_TYPE_FILM = "film";
+}
---
+++ tests/API/SearchVideosRequestInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the pixabay-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pixabay\Tests\API;
+
+use WBW\Library\Pixabay\API\SearchVideosRequestInterface;
+use WBW\Library\Pixabay\Tests\AbstractTestCase;
+
+/**
+ * Search videos request interface test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pixabay\Tests\API
+ */
+class SearchVideosRequestInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $this->assertEquals("all", SearchVideosRequestInterface::VIDEO_TYPE_ALL);
+        $this->assertEquals("animation", SearchVideosRequestInterface::VIDEO_TYPE_ANIMATION);
+        $this->assertEquals("film", SearchVideosRequestInterface::VIDEO_TYPE_FILM);
+    }
+}
