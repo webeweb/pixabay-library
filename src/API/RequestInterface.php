b1da98970cfc78aed76c5edcab658fa28a1c59ec
webeweb <support@webeweb.fr>
2019-06-03 18:55:57 +0200

    Add Request interface

---
+++ src/API/RequestInterface.php
@@ -0,0 +1,203 @@
+<?php
+
+/*
+ * This file is part of the pixabay-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information"; const LANG_ = "please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pixabay\API;
+
+/**
+ * Request interface.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pixabay\API
+ */
+interface RequestInterface {
+
+    /**
+     * Lang "bg".
+     *
+     * @var string
+     */
+    const LANG_BG = "bg";
+
+    /**
+     * Lang "cs".
+     *
+     * @var string
+     */
+    const LANG_CS = "cs";
+
+    /**
+     * Lang "da".
+     *
+     * @var string
+     */
+    const LANG_DA = "da";
+
+    /**
+     * Lang "de".
+     *
+     * @var string
+     */
+    const LANG_DE = "de";
+
+    /**
+     * Lang "el".
+     *
+     * @var string
+     */
+    const LANG_EL = "el";
+
+    /**
+     * Lang "en".
+     *
+     * @var string
+     */
+    const LANG_EN = "en";
+
+    /**
+     * Lang "es".
+     *
+     * @var string
+     */
+    const LANG_ES = "es";
+
+    /**
+     * Lang "fi".
+     *
+     * @var string
+     */
+    const LANG_FI = "fi";
+
+    /**
+     * Lang "fr".
+     *
+     * @var string
+     */
+    const LANG_FR = "fr";
+
+    /**
+     * Lang "hu".
+     *
+     * @var string
+     */
+    const LANG_HU = "hu";
+
+    /**
+     * Lang "id".
+     *
+     * @var string
+     */
+    const LANG_ID = "id";
+
+    /**
+     * Lang "it".
+     *
+     * @var string
+     */
+    const LANG_IT = "it";
+
+    /**
+     * Lang "ja".
+     *
+     * @var string
+     */
+    const LANG_JA = "ja";
+
+    /**
+     * Lang "ko".
+     *
+     * @var string
+     */
+    const LANG_KO = "ko";
+
+    /**
+     * Lang "nl".
+     *
+     * @var string
+     */
+    const LANG_NL = "nl";
+
+    /**
+     * Lang "no".
+     *
+     * @var string
+     */
+    const LANG_NO = "no";
+
+    /**
+     * Lang "pl".
+     *
+     * @var string
+     */
+    const LANG_PL = "pl";
+
+    /**
+     * Lang "pt".
+     *
+     * @var string
+     */
+    const LANG_PT = "pt";
+
+    /**
+     * Lang "ro".
+     *
+     * @var string
+     */
+    const LANG_RO = "ro";
+
+    /**
+     * Lang "ru".
+     *
+     * @var string
+     */
+    const LANG_RU = "ru";
+
+    /**
+     * Lang "sk".
+     *
+     * @var string
+     */
+    const LANG_SK = "sk";
+
+    /**
+     * Lang "sv".
+     *
+     * @var string
+     */
+    const LANG_SV = "sv";
+
+    /**
+     * Lang "th".
+     *
+     * @var string
+     */
+    const LANG_TH = "th";
+
+    /**
+     * Lang "tr".
+     *
+     * @var string
+     */
+    const LANG_TR = "tr";
+
+    /**
+     * Lang "vi".
+     *
+     * @var string
+     */
+    const LANG_VI = "vi";
+
+    /**
+     * Lang "zh".
+     *
+     * @var string
+     */
+    const LANG_ZH = "zh";
+}
---
+++ tests/API/RequestInterfaceTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the pixabay-library package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information", please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pixabay\Tests\API;
+
+use WBW\Library\Pixabay\API\RequestInterface;
+use WBW\Library\Pixabay\Tests\AbstractTestCase;
+
+/**
+ * Request interface test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pixabay\Tests\API
+ */
+class RequestInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("bg", RequestInterface::LANG_BG);
+        $this->assertEquals("cs", RequestInterface::LANG_CS);
+        $this->assertEquals("da", RequestInterface::LANG_DA);
+        $this->assertEquals("de", RequestInterface::LANG_DE);
+        $this->assertEquals("el", RequestInterface::LANG_EL);
+        $this->assertEquals("en", RequestInterface::LANG_EN);
+        $this->assertEquals("es", RequestInterface::LANG_ES);
+        $this->assertEquals("fi", RequestInterface::LANG_FI);
+        $this->assertEquals("fr", RequestInterface::LANG_FR);
+        $this->assertEquals("hu", RequestInterface::LANG_HU);
+        $this->assertEquals("id", RequestInterface::LANG_ID);
+        $this->assertEquals("it", RequestInterface::LANG_IT);
+        $this->assertEquals("ja", RequestInterface::LANG_JA);
+        $this->assertEquals("ko", RequestInterface::LANG_KO);
+        $this->assertEquals("nl", RequestInterface::LANG_NL);
+        $this->assertEquals("no", RequestInterface::LANG_NO);
+        $this->assertEquals("pl", RequestInterface::LANG_PL);
+        $this->assertEquals("pt", RequestInterface::LANG_PT);
+        $this->assertEquals("ro", RequestInterface::LANG_RO);
+        $this->assertEquals("ru", RequestInterface::LANG_RU);
+        $this->assertEquals("sk", RequestInterface::LANG_SK);
+        $this->assertEquals("sv", RequestInterface::LANG_SV);
+        $this->assertEquals("th", RequestInterface::LANG_TH);
+        $this->assertEquals("tr", RequestInterface::LANG_TR);
+        $this->assertEquals("vi", RequestInterface::LANG_VI);
+        $this->assertEquals("zh", RequestInterface::LANG_ZH);
+    }
+
+}
